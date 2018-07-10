9480fb08bb3cf6a8e83969a67113d20352d20486
Radhika Gupta <radhika.gupta@yahoo.com>
2018-07-10 09:23:30 -0400

    completed routes

---
+++ server/index.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+const handle = require('./handlers');
+const db = require('./models');
+const routes = require('./routes');
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get('/', (req, res)=>res.json({hello: 'world'}));
+app.use('/api/auth', routes.auth);
+app.use('/api/polls', routes.poll);
+
+
+app.use(handle.notFound);
+app.use(handle.errors);
+
+app.listen(port, console.log(`Server listening on port ${port}`)); 
---
+++ server/routes/auth.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const handle = require('../handlers');
+
+router.post('/login', handle.login);
+router.post('/register', handle.register);
+
+module.exports = router;
